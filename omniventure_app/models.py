08a8cb327e473cad4ff274da2d971d033a77f018
Luke Haas <lhaas@uccs.edu>
2024-04-01 10:30:01 -0600

    Template page working, models made.

--- omniventure_app/models.py
+++ omniventure_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Member(models.Model):
+    image = models.ImageField()
+    name = models.CharField(max_length = 200, blank = False)
+    pronouns = models.CharField(max_length = 40)
+    active_times = models.DateTimeField()
+    main_character = 0 #placeholder
+    about = models.CharField(max_length = 200)
+    discord = models.CharField(max_length = 100)
+    discord_message_check = models.BooleanField(default = False)
+    is_active = models.BooleanField(default = False)
