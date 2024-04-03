b3bafe18de92c330e6eaae0f077782f1bf6b6d23
Luke Haas <lhaas@uccs.edu>
2024-04-03 09:58:28 -0600

    Member list/detail views complete with images

--- omniventure_app/models.py
+++ omniventure_app/models.py
@@ -1,14 +1,40 @@
 from django.db import models
 from django.urls import reverse
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Member(models.Model):
-    image = models.ImageField()
-    name = models.CharField(max_length = 200, blank = False)
-    pronouns = models.CharField(max_length = 40)
-    active_times = models.DateTimeField()
+
+    def validate_image_size(image):
+        width, height = image.width, image.height
+        if width > 1000 or height > 1000:  # Set your desired maximum width and height
+            raise ValidationError(
+                _('The image dimensions exceed the allowed size of 1000x1000 pixels.')
+        )
+    
+    def user_directory_path(instance, filename): 
+  
+        # file will be uploaded to MEDIA_ROOT / user_<id>/<filename> 
+        return 'member_{0}/{1}'.format(instance.pk, filename) 
+    
+    image = models.ImageField(upload_to=user_directory_path,
+        blank=True,
+         validators=[validate_image_size]
+    )
+    name = models.CharField(max_length = 200, blank = False, primary_key = True)
+    pronouns = models.CharField(max_length = 40, blank = True)
+    active_times = models.CharField(max_length = 200, blank = True)
     main_character = 0 #placeholder
-    about = models.CharField(max_length = 200)
-    discord = models.CharField(max_length = 100)
+    about = models.CharField(max_length = 200, blank = True)
+    discord = models.CharField(max_length = 100, blank = True)
     discord_message_check = models.BooleanField(default = False)
     is_active = models.BooleanField(default = False)
+
+def __str__(self):
+    return self.title
+    
+def get_absolute_url(self):
+    return reverse('member_detail', args=[str(self.id)])
+
+
--- omniventure_app/views.py
+++ omniventure_app/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from .models import *
+from django.views import generic
+#from .forms import *
+from django.shortcuts import redirect
 # Create your views here.
 def index(request):
 
     return render( request, 'omniventure_app/index.html')
+
+class member_list(generic.ListView):
+    model = Member
+
+class member_detail(generic.DetailView):
+    model = Member
